a80b05287839eba07514588cecfe92a736d8725d
Brianna Dukes <skylar.deep@yahoo.com>
2022-10-17 06:38:26 -0400

    update nav, install bootstrap

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,17 +10,17 @@ export default function NavBar({user, setUser}) {
     }
 // setting up the nav bar links and their routes
     return (
-        <nav>
+        <nav justify variant="tabs" >
             <span>Welcome, {user.name}</span>
-            &nbsp; | &nbsp;
+                &nbsp; | &nbsp;
             <Link to="/menu">Menu</Link>
-            &nbsp; | &nbsp;
+                &nbsp; | &nbsp;
             <Link to="/about">Our Story</Link>
-            &nbsp; | &nbsp;
+                &nbsp; | &nbsp;
             <Link to="/orders/new">Order Page</Link>
-            &nbsp; | &nbsp;
+                &nbsp; | &nbsp;
             <HashLink smooth to="/#booking">Book a Table</HashLink>
-            &nbsp; | &nbsp;
+                &nbsp; | &nbsp;
             <button onClick={handleLogOut} type="button" className="btn btn-dark">Logout</button>
         </nav>
     )
