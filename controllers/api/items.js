30d707bbd535cfd8611d2f785e16aa68ea129fa3
Brianna Dukes <skylar.deep@yahoo.com>
2022-10-17 04:29:09 -0400

    update readme

--- controllers/api/items.js
+++ controllers/api/items.js
@@ -1,6 +1,4 @@
 const Item = require('../../models/item');
-const fetch = require('node-fetch');
-const rootAPIURL = 'https://api.edamam.com/api/recipes/v2?type=public&q=steak&app_id=d64967dc&app_key=a5f9f1786ee37fd224fadf684361a6d3';
 
 async function index(req, res) {
   const items = await Item.find({}).sort('name').populate('category').exec();
