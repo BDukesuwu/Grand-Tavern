52d1c4e56afa3984c8cc5f8443f18b5d7015bbaf
Brianna Dukes <skylar.deep@yahoo.com>
2022-10-17 17:33:29 -0400

    update css

--- src/pages/MenuPage/MenuPage.jsx
+++ src/pages/MenuPage/MenuPage.jsx
@@ -11,38 +11,46 @@ export default function MenuPage() {
   return (
     <main className="MenuPage">
       <div className="row">
-        <h1>Our Menu</h1>
+        <h1>Popular Dinners</h1>
       </div>
 
       <div className="column">
         <img src={steakbites} />
         <h3>Steak Bites</h3>
+        <h5>25</h5>
       </div>
 
       <div className="column">
         <img src={pansear} />
         <h3>Pan-Fried Steak</h3>
+        <h5>32</h5>
       </div>
       
       <div className="column">
         <img src={teriyaki} />
         <h3>Teriyaki Steak </h3>
+        <h5>25</h5>
       </div>
       
       <div className="column">
         <img src={sugarsteak} />
         <h3>Vietnamese Sugar Steak</h3>
+        <h5>30</h5>
       </div>
       
       <div className="column">
         <img src={flank} />
         <h3>Panfried Flank Steak</h3>
+        <h5>28</h5>
       </div>
       
       <div className="column">
         <img src={sous} />
         <h3>Sous-vide steak</h3>
+        <h5>30</h5>
       </div>
+
+    <h2>For more Menu Items, see the order page!</h2>
     </main>
   );
 }
